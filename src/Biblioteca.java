aa06d7b53423d5aa3b7b0b0ba6f205a2d8e64c47
Mario <mreald05@iesalbarregas.es>
2026-03-11 11:29:55 +0100

    Version 2

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -2,5 +2,15 @@ public class Biblioteca {
     private Libro[] libros;
 
     public Biblioteca() {
+        libros = new Libro[10];
+    }
+
+    public void agregarLibro(Libro libro) {
+        for (int i = 0; i < libros.length; i++) {
+            if (libros[i]==null) {
+                libros[i]=libro;
+                break;
+            }
+        }
     }
 }
